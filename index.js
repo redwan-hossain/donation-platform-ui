1ce0a7a0f81a56e184ad42de81086a39a4c7df11
redwan-hossain <rfredwan0@gmail.com>
2026-01-19 20:58:17 +0600

    donate plan section added

---
+++ index.js
@@ -0,0 +1,29 @@
+const loadDonatePlanData = () => {
+    fetch("donate.json")
+        .then(res => res.json())
+        .then(data => {
+            showDonateData(data);
+        })
+}
+
+
+const showDonateData = (data) => {
+    let donateContainer = document.querySelector(".donate-container");
+
+    data.forEach(element => {
+
+        donateContainer.innerHTML += `
+       
+         <div class="donate-card bg-[rgba(79,157,166,0.1)] w-[360px] max-h-[400px] rounded-md p-10 space-y-5">
+                    <h1 class="text-5xl">${element.icon}</h1>
+                    <h1 class="font-semibold text-3xl">${element.title}</h1>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat
+                        duis enim velit mollit.</p>
+                </div>
+                
+                `
+    });
+}
+
+
+loadDonatePlanData();
